8f17bdca6f95e4097b1718592f6e8733212d333f
saipasumarthy <saipasumarthy009@gmail.com>
2023-02-03 13:33:00 +0530

    Created FeedItem

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sai Pasumarthy on 03/02/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
