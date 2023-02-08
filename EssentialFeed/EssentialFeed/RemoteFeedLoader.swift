959dad4fac26c41c551f1dbcced240e9a011956f
saipasumarthy <saipasumarthy009@gmail.com>
2023-02-08 17:10:37 +0530

    Delivers invalidData error on 200 HTTPResponse with invalid JSON

--- EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum HTTPClientResult {
-    case success(HTTPURLResponse)
+    case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -59,6 +59,17 @@ class RemoteFeedLoaderTests: XCTestCase {
             XCTAssertEqual(capturedError, [.invalidData])
         }
     }
+    
+    func test_load_deliversStatusCode200WithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load { capturedError.append($0) }
+
+        let invalidJSON = Data.init("invalidJSON".utf8)
+        
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -81,9 +92,9 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0 ) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0 ) {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }
