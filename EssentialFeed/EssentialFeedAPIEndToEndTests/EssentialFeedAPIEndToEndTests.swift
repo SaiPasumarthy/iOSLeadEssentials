611de21f81661eb66b2037545d20b5047b39fdd6
saipasumarthy <saipasumarthy009@gmail.com>
2023-02-12 13:18:00 +0530

    Added memory leak for E2E tests

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -17,16 +17,18 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             case let .failure(error):
                 XCTFail("Expected success but got \(error) instead")
             default:
-                XCTFail("Expected success but got failure instead")
+                XCTFail("Expected success but got no result instead")
         }
     }
     
     //MARK: - Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let serverUrl = URL(string: "https://www.essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: serverUrl, client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         var receivedResult: LoadFeedResult?
         let exp = expectation(description: "Wait for expectation")
         loader.load { feedResult in
