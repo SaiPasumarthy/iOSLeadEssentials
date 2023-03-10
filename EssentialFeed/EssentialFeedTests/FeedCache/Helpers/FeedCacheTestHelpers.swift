18615d818a3c850c32dd601211eacb945e509d31
saipasumarthy <saipasumarthy009@gmail.com>
2023-03-10 18:12:43 +0530

    Separated date extension

--- EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
@@ -30,7 +30,8 @@ extension Date {
     private func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
-    
+}
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
