baed875dae27e338607453d2da6d0e4cce45bb54
saipasumarthy <saipasumarthy009@gmail.com>
2023-03-11 12:26:50 +0530

    Move codable conformance to new framework specific feed image type

--- EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -9,24 +9,50 @@ import XCTest
 import EssentialFeed
 
 class CodableFeedStore {
-    private struct Cache: Codable {
-        let feed: [LocalFeedImage]
-        let timestamp: Date
+    private struct CodableFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+        
+        var local: LocalFeedImage {
+            return LocalFeedImage(id: id, description: description, location: location, url: url)
+        }
+        
+        init(_ image: LocalFeedImage) {
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
     }
-    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    private let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+    private struct Cache: Codable {
+        let feed: [CodableFeedImage]
+        let timestamp: Date
+        var localFeed: [LocalFeedImage] {
+            feed.map { $0.local }
+        }
+    }
+    
     
     func retrieval(with completion: @escaping FeedStore.RetrievalCompletion) {
         let decoder = JSONDecoder()
         guard let data = try? Data(contentsOf: storeURL) else { return completion(.empty) }
         
         let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(images: cache.feed, timestamp: cache.timestamp))
+        completion(.found(images: cache.localFeed, timestamp: cache.timestamp))
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date,
                 completion: @escaping FeedStore.InsertionCompletion) {
         let encoder = JSONEncoder()
-        let data = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+        let data = try! encoder.encode(cache)
         try! data.write(to: storeURL)
         completion(nil)
     }
@@ -37,14 +63,12 @@ class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
         try? FileManager.default.removeItem(at: storeURL)
     }
     
     override func tearDown() {
         super.tearDown()
         
-        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
         try? FileManager.default.removeItem(at: storeURL)
     }
     
@@ -104,7 +128,9 @@ class CodableFeedStoreTests: XCTestCase {
     
     //MARK: Helpers
     
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+
     func makeSUT() -> CodableFeedStore {
-        return CodableFeedStore()
+        return CodableFeedStore(storeURL: storeURL)
     }
 }
