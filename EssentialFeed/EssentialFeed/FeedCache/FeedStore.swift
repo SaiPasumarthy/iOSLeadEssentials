9d9a958fe7bcfeb9e381f81644ec2774efa0dcf8
saipasumarthy <saipasumarthy009@gmail.com>
2023-03-12 12:53:40 +0530

    Added comment for protocol methods

--- EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -18,7 +18,15 @@ public protocol FeedStore {
     typealias InsertionCompletion = ((Error?) -> Void)
     typealias RetrievalCompletion = ((RetrievalCachedFeedResult) -> Void)
     
+    /// The Completion handler can be invoke in any thread
+    /// Clients are responsible to dispatch to appropriate thread, if needed
     func deleteCachedFeed(with completion: @escaping DeletionCompletion)
+    
+    /// The Completion handler can be invoke in any thread
+    /// Clients are responsible to dispatch to appropriate thread, if needed
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The Completion handler can be invoke in any thread
+    /// Clients are responsible to dispatch to appropriate thread, if needed
     func retrieval(with completion: @escaping RetrievalCompletion)
 }
