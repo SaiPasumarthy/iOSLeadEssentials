c469ebe3a5d7ae0d0ff33df481adcff5aba62c5b
saipasumarthy <saipasumarthy009@gmail.com>
2023-05-12 09:14:21 +0530

    Extract duplicate HTTPClientSpy helper into a shared scope

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -193,36 +193,5 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         action()
         
         wait(for: [expectation], timeout: 1.0)
-        
-    }
-    
-    private class HTTPClientSpy: HTTPClient {
-        private struct Task: HTTPClientTask {
-            let callback: () -> Void
-            func cancel() {
-                callback()
-            }
-        }
-        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        var cancelledURLs = [URL]()
-        var requestedURLs: [URL] {
-            messages.map { $0.url }
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let httpResponse = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success((data, httpResponse)))
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task { [weak self] in
-                self?.cancelledURLs.append(url)
-            }
-        }
     }
 }
--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -149,27 +149,4 @@ class RemoteFeedLoaderTests: XCTestCase {
         let itemsJson = ["items" : items]
         return try! JSONSerialization.data(withJSONObject: itemsJson)
     }
-    private class HTTPClientSpy: HTTPClient {
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-        var requestedURLs: [URL] {
-            return messages.map { $0.url }
-        }
-        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task()
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0 ) {
-            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success((data, response)))
-        }
-    }
 }
