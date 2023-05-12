e5e0daeca0c7eee0f0e72830049a6f1fc8b26101
saipasumarthy <saipasumarthy009@gmail.com>
2023-05-12 09:28:54 +0530

    Refactor switch statement with map and flatMap chain

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedImageDataLoader.swift
@@ -45,16 +45,13 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
             
-            switch result {
-            case  let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { (data, response) in
+                    let validResponse = response.statusCode == 200 && !data.isEmpty
+                    return validResponse ? .success(data) : .failure(Error.invalidData)
                 }
-            case .failure:
-                task.complete(with: .failure(Error.connectivity))
-            }
+            )
         }
         
         return task
