e1637d3b3542ebe813062fded3937da731aa4927
saipasumarthy <saipasumarthy009@gmail.com>
2023-06-03 12:15:27 +0530

    Add test to guarantee that CoreDataFeedStore side effects run serially to prevent unexpected behavior

--- EssentialFeed/EssentialFeedTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
@@ -48,6 +48,28 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteWith: found(lastStoredData), for: matchingURL)
     }
     
+    func test_sideEffects_runSerially() {
+        let sut = makeSUT()
+        let url = anyURL()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert([localImage(url: url)], timestamp: Date()) { _ in
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.insert(data: anyData(), for: url) { _ in
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(data: anyData(), for: url) { _ in
+            op3.fulfill()
+        }
+        
+        wait(for: [op1, op2, op3], timeout: 5.0, enforceOrder: true)
+    }
+    
     // - MARK: Helpers
     
     private func found(_ data: Data) -> FeedImageDataStore.RetrievalResult {
@@ -96,16 +118,16 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
             switch result {
             case let .failure(error):
                 XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+                exp.fulfill()
                 
             case .success:
                 sut.insert(data: data, for: url) { result in
                     if case let Result.failure(error) = result {
                         XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
                     }
+                    exp.fulfill()
                 }
             }
-            
-            exp.fulfill()
         }
                 
         wait(for: [exp], timeout: 1.0)
