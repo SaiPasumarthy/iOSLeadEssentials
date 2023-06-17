e41b43ae82c8529b74768c91b8442a7988d0058f
saipasumarthy <saipasumarthy009@gmail.com>
2023-06-17 20:02:17 +0530

    Move HTTPClientStub to a shared scope in a separate file

---
+++ EssentialAppTests/Helpers/HTTPClientStub.swift
@@ -0,0 +1,34 @@
+//
+//  HTTPClientStub.swift
+//  EssentialAppTests
+//
+//  Created by Sai Pasumarthy on 17/06/23.
+//
+
+import Foundation
+import EssentialFeed
+
+class HTTPClientStub: HTTPClient {
+    private class Task: HTTPClientTask {
+        func cancel() {}
+    }
+    
+    private let stub: (URL) -> HTTPClient.Result
+    
+    init(stub: @escaping (URL) -> HTTPClient.Result) {
+        self.stub = stub
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        completion(stub(url))
+        return Task()
+    }
+    
+    static var offline: HTTPClientStub {
+        HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
+    }
+    
+    static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> HTTPClientStub {
+        HTTPClientStub { url in .success(stub(url)) }
+    }
+}
