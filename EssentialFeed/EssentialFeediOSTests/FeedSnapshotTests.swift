ca1cb8e01010fa9cfd7196e2a3891264f63b976d
saipasumarthy <saipasumarthy009@gmail.com>
2023-06-18 17:04:12 +0530

    Fixed test_feedWithErrorMessage

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -28,14 +28,14 @@ class FeedSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .light)), named: "FEED_WITH_CONTENT_light")
     }
     
-//    func test_feedWithErrorMessage() {
-//        let sut = makeSUT()
-//        
-//        sut.display(viewModel: .error(message: "This is a\nmulti-line\nerror message"))
-//        
-//        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
-//        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
-//    }
+    func test_feedWithErrorMessage() {
+        let sut = makeSUT()
+        
+        sut.display(viewModel: .error(message: "This is a\nmulti-line\nerror message"))
+        
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
+    }
     
     func test_feedWithFailedImageLoading() {
         let sut = makeSUT()
