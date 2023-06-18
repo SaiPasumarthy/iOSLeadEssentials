d0aa3d5243c5569cc26346b4cd4e814d17fb130f
saipasumarthy <saipasumarthy009@gmail.com>
2023-06-18 16:24:59 +0530

    Move UIViewController snapshot helpers to separate file

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
@@ -0,0 +1,67 @@
+//
+//  UIViewController+Snapshot.swift
+//  EssentialFeediOSTests
+//
+//  Created by Sai Pasumarthy on 18/06/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func snapshot(for configuration: SnapshotConfiguration) -> UIImage {
+        return SnapshotWindow(configuration: configuration, root: self).snapshot()
+    }
+}
+
+struct SnapshotConfiguration {
+     let size: CGSize
+     let safeAreaInsets: UIEdgeInsets
+     let layoutMargins: UIEdgeInsets
+     let traitCollection: UITraitCollection
+
+     static func iPhone13Mini(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
+         return SnapshotConfiguration(
+             size: CGSize(width: 360, height: 780),
+             safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
+             layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
+             traitCollection: UITraitCollection(traitsFrom: [
+                 .init(forceTouchCapability: .available),
+                 .init(layoutDirection: .leftToRight),
+                 .init(preferredContentSizeCategory: .medium),
+                 .init(userInterfaceIdiom: .phone),
+                 .init(horizontalSizeClass: .compact),
+                 .init(verticalSizeClass: .regular),
+                 .init(displayScale: 3),
+                 .init(displayGamut: .P3),
+                 .init(userInterfaceStyle: style)
+             ]))
+     }
+ }
+
+private final class SnapshotWindow: UIWindow {
+     private var configuration: SnapshotConfiguration = .iPhone13Mini(style: .light)
+
+     convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
+         self.init(frame: CGRect(origin: .zero, size: configuration.size))
+         self.configuration = configuration
+         self.layoutMargins = configuration.layoutMargins
+         self.rootViewController = root
+         self.isHidden = false
+         root.view.layoutMargins = configuration.layoutMargins
+     }
+
+     override var safeAreaInsets: UIEdgeInsets {
+         return configuration.safeAreaInsets
+     }
+
+     override var traitCollection: UITraitCollection {
+         return UITraitCollection(traitsFrom: [super.traitCollection, configuration.traitCollection])
+     }
+
+     func snapshot() -> UIImage {
+         let renderer = UIGraphicsImageRenderer(bounds: bounds, format: .init(for: traitCollection))
+         return renderer.image { action in
+             layer.render(in: action.cgContext)
+         }
+     }
+ }
