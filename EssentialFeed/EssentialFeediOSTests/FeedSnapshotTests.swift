d50e0d2a82e0725c7118144357be6c7cea037aef
saipasumarthy <saipasumarthy009@gmail.com>
2023-06-18 16:21:55 +0530

    Assert snapshots for light and dark mode using an iPhone8 configuration

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -15,7 +15,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(emptyFeed())
         
-        assert(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .dark)), named: "EMPTY_FEED_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .light)), named: "EMPTY_FEED_light")
     }
     
     func test_feedWithContent() {
@@ -23,7 +24,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(feedWithContent())
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .dark)), named: "FEED_WITH_CONTENT_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .light)), named: "FEED_WITH_CONTENT_light")
     }
     
     func test_feedWithErrorMessage() {
@@ -31,7 +33,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(viewModel: .error(message: "This is a\nmulti-line\nerror message"))
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
     }
     
     func test_feedWithFailedImageLoading() {
@@ -39,7 +42,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(feedWithFailedImageLoading())
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13Mini(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
     }
 
     // MARK: - Helpers
@@ -48,6 +52,8 @@ class FeedSnapshotTests: XCTestCase {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let controller = storyboard.instantiateInitialViewController() as! FeedViewController
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
         return controller
     }
     
@@ -138,13 +144,63 @@ class FeedSnapshotTests: XCTestCase {
 }
 
 extension UIViewController {
-    func snapshot() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
-        return renderer.image { action in
-            view.layer.render(in: action.cgContext)
-        }
+    func snapshot(for configuration: SnapshotConfiguration) -> UIImage {
+        return SnapshotWindow(configuration: configuration, root: self).snapshot()
     }
 }
+
+struct SnapshotConfiguration {
+     let size: CGSize
+     let safeAreaInsets: UIEdgeInsets
+     let layoutMargins: UIEdgeInsets
+     let traitCollection: UITraitCollection
+
+     static func iPhone13Mini(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
+         return SnapshotConfiguration(
+             size: CGSize(width: 360, height: 780),
+             safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
+             layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
+             traitCollection: UITraitCollection(traitsFrom: [
+                 .init(forceTouchCapability: .available),
+                 .init(layoutDirection: .leftToRight),
+                 .init(preferredContentSizeCategory: .medium),
+                 .init(userInterfaceIdiom: .phone),
+                 .init(horizontalSizeClass: .compact),
+                 .init(verticalSizeClass: .regular),
+                 .init(displayScale: 3),
+                 .init(displayGamut: .P3),
+                 .init(userInterfaceStyle: style)
+             ]))
+     }
+ }
+
+private final class SnapshotWindow: UIWindow {
+     private var configuration: SnapshotConfiguration = .iPhone13Mini(style: .light)
+
+     convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
+         self.init(frame: CGRect(origin: .zero, size: configuration.size))
+         self.configuration = configuration
+         self.layoutMargins = configuration.layoutMargins
+         self.rootViewController = root
+         self.isHidden = false
+         root.view.layoutMargins = configuration.layoutMargins
+     }
+
+     override var safeAreaInsets: UIEdgeInsets {
+         return configuration.safeAreaInsets
+     }
+
+     override var traitCollection: UITraitCollection {
+         return UITraitCollection(traitsFrom: [super.traitCollection, configuration.traitCollection])
+     }
+
+     func snapshot() -> UIImage {
+         let renderer = UIGraphicsImageRenderer(bounds: bounds, format: .init(for: traitCollection))
+         return renderer.image { action in
+             layer.render(in: action.cgContext)
+         }
+     }
+ }
 
 private extension FeedViewController {
     func display(_ stubs: [ImageStub]) {
