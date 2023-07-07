b24cb332b59d6aab53fa8889c6e9b26840506103
saipasumarthy <saipasumarthy009@gmail.com>
2023-07-07 16:09:37 +0530

    Guarantee Main Dispatch Queue on custom Scheduler

--- EssentialApp/CombineHelpers.swift
+++ EssentialApp/CombineHelpers.swift
@@ -76,7 +76,7 @@ public extension FeedImageDataLoader {
 
  extension DispatchQueue {
      static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler {
-         ImmediateWhenOnMainQueueScheduler()
+         ImmediateWhenOnMainQueueScheduler.shared
      }
 
      struct ImmediateWhenOnMainQueueScheduler: Scheduler {
@@ -91,8 +91,21 @@ public extension FeedImageDataLoader {
              DispatchQueue.main.minimumTolerance
          }
 
+         static let shared = Self()
+         
+         private static let key = DispatchSpecificKey<UInt8>()
+         private static let value = UInt8.max
+         
+         private init() {
+             DispatchQueue.main.setSpecific(key: Self.key, value: Self.value)
+         }
+         
+         private func isMainQueue() -> Bool {
+             DispatchQueue.getSpecific(key: Self.key) == Self.value
+         }
+         
          func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
-             guard Thread.isMainThread else {
+             guard isMainQueue() else {
                  return DispatchQueue.main.schedule(options: options, action)
              }
 
