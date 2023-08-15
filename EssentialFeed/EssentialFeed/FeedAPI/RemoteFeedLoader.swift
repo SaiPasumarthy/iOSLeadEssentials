664a4b7e258e9137fcc1e3bf9fda1428c1716725
saipasumarthy <saipasumarthy009@gmail.com>
2023-08-15 14:57:39 +0530

    Replace RemoteFeedLoader with generic RemoteLoader to remove duplication

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -7,45 +7,14 @@
 
 import Foundation
 
-#warning("How client of RemoteFeedLoader doesn't knows about HTTPClient when it dependancy injects through init")
-#warning("Who is the client of RemoteFeedLoader? Is it whoever instantiate the RemoteFeedLoader class?")
-public final class RemoteFeedLoader: FeedLoader {
-    private let url: URL
-    private let client: HTTPClient
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
+public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
 
-    public typealias Result = FeedLoader.Result
-
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-#warning("URL is detail implementation and shouldn't be in public interface. But the doubt is how does every client of RemoteFeedLoader know of url to request data from")
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else {
-                return
-            }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteFeedLoader.map(data, with: response))
-                
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map(_ data: Data, with response: HTTPURLResponse) -> Result {
-        do {
-            let items = try FeedItemMapper.map(data, from: response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+public extension RemoteFeedLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: FeedItemMapper.map)
     }
 }
+
+#warning("How client of RemoteFeedLoader doesn't knows about HTTPClient when it dependancy injects through init")
+#warning("Who is the client of RemoteFeedLoader? Is it whoever instantiate the RemoteFeedLoader class?")
+#warning("URL is detail implementation and shouldn't be in public interface. But the doubt is how does every client of RemoteFeedLoader know of url to request data from")
