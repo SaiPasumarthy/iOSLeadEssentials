209891a5fb59868a4a5588e354828b2e8c83d5dd
saipasumarthy <saipasumarthy009@gmail.com>
2023-10-23 16:50:37 +0530

    Inject currentDate, locale, and calendar to make tests reliable in any locale

--- EssentialFeed/EssentialFeed/ImageCommentsPresentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/ImageCommentsPresentation/ImageCommentsPresenter.swift
@@ -7,11 +7,45 @@
 
 import Foundation
 
+public struct ImageCommentsViewModel: Equatable {
+    public let comments: [ImageCommentViewModel]
+}
+
+public struct ImageCommentViewModel: Equatable {
+    let message: String
+    let date: String
+    let username: String
+    
+    public init(message: String, date: String, username: String) {
+        self.message = message
+        self.date = date
+        self.username = username
+    }
+}
+
 public final class ImageCommentsPresenter {
     public static var title: String {
         return NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
                                  tableName: "ImageComments",
                                  bundle: Bundle(for: Self.self),
                                  comment: "Title for the feed view")
+    }
+    
+    public static func map(
+        _ comments: [ImageComment],
+        currentDate: Date = Date(),
+        calendar: Calendar = .current,
+        locale: Locale = .current
+    ) -> ImageCommentsViewModel {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.calendar = calendar
+        formatter.locale = locale
+        return  ImageCommentsViewModel(comments: comments.map { comment in
+            ImageCommentViewModel(
+                message: comment.message,
+                date: formatter.localizedString(for: comment.createdAt, relativeTo: currentDate),
+                username: comment.username
+            )
+        })
     }
 }
--- EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
@@ -14,6 +14,28 @@ final class ImageCommentsPresenterTests: XCTestCase {
         XCTAssertEqual(ImageCommentsPresenter.title, localise("IMAGE_COMMENTS_VIEW_TITLE"))
     }
     
+    func test_map_createViewModels() {
+        let now = Date()
+        let calendar = Calendar(identifier: .gregorian)
+        let locale = Locale(identifier: "en_US_POSIX")
+        let comments = [
+            ImageComment(id: UUID(), message: "a message", createdAt: now.adding(minutes: -5), username: "a username"),
+            ImageComment(id: UUID(), message: "another message", createdAt: now.adding(days: -1), username: "another username")
+        ]
+        
+        let viewModel = ImageCommentsPresenter.map(
+            comments,
+            currentDate: now,
+            calendar: calendar,
+            locale: locale
+        )
+        
+        XCTAssertEqual(viewModel.comments, [
+            ImageCommentViewModel(message: "a message", date: "5 minutes ago", username: "a username"),
+            ImageCommentViewModel(message: "another message", date: "1 day ago", username: "another username")
+        ])
+    }
+    
     private func localise(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
         let table: String = "ImageComments"
         let bundle = Bundle(for: ImageCommentsPresenter.self)
