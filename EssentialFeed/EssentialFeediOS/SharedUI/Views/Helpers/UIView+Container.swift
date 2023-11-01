7a1d25dee449ce6760c02852f0dfd56e6faee004
saipasumarthy <saipasumarthy009@gmail.com>
2023-11-01 11:30:50 +0530

    Add UIView.makeContainer helper

---
+++ EssentialFeed/EssentialFeediOS/SharedUI/Views/Helpers/UIView+Container.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Container.swift
+//  EssentialFeediOS
+//
+//  Created by Sai Pasumarthy on 01/11/23.
+//
+
+import UIKit
+
+extension UIView {
+
+    public func makeContainer() -> UIView {
+        let container = UIView()
+        container.backgroundColor = .clear
+        container.addSubview(self)
+
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: trailingAnchor),
+            topAnchor.constraint(equalTo: container.topAnchor),
+            container.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+
+        return container
+    }
+
+}
