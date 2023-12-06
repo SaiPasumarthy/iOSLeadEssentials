820520372b1ae6f44363561c377bc6fad543505c
saipasumarthy <saipasumarthy009@gmail.com>
2023-12-06 21:40:58 +0530

    Cache page results

--- EssentialApp/CombineHelpers.swift
+++ EssentialApp/CombineHelpers.swift
@@ -99,8 +99,12 @@ public extension FeedImageDataLoader {
      }
  }
 
- extension Publisher where Output == [FeedImage] {
-     func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> {
+ extension Publisher {
+     func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> where Output == [FeedImage] {
+         handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
+     }
+     
+     func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> where Output == Paginated<FeedImage> {
          handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
      }
  }
@@ -108,6 +112,10 @@ public extension FeedImageDataLoader {
  private extension FeedCache {
      func saveIgnoringResult(_ feed: [FeedImage]) {
          save(feed) { _ in }
+     }
+     
+     func saveIgnoringResult(_ page: Paginated<FeedImage>) {
+         saveIgnoringResult(page.items)
      }
  }
 
