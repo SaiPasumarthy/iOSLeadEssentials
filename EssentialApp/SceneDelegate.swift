21443917e1f3e1a7251aa12b5807316c0b53943a
saipasumarthy <saipasumarthy009@gmail.com>
2023-12-09 15:21:27 +0530

    Moved the Combine logging into extenstions

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -127,23 +127,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         return localImageLoader
             .loadImageDataPublisher(from: url)
+            .logCacheMisses(url: url, logger: logger)
             .fallback { [httpClient, logger] in
-                var startTime = CACurrentMediaTime()
-                return httpClient
+                httpClient
                     .getPublisher(url: url)
-                    .handleEvents(receiveSubscription: { _ in
-                        startTime = CACurrentMediaTime()
-                        logger.trace("Started loading url: \(url)")
-                    }, receiveCompletion: { result in
-                        if case let .failure(error) = result {
-                            logger.trace("Failed to loading url: \(url) with error: \(error.localizedDescription)")
-                        }
-                        
-                        let elasped = CACurrentMediaTime() - startTime
-                        logger.trace("Finished loading url: \(url) in \(elasped) seconds")
-                    })
+                    .logErrors(url: url, logger: logger)
+                    .logElapsedTime(url: url, logger: logger)
                     .tryMap(FeedImageDataMapper.map)
                     .caching(to: localImageLoader, using: url)
             }
+    }
+}
+
+extension Publisher {
+    func logCacheMisses(url: URL, logger: Logger) -> AnyPublisher<Output, Failure> {
+        return handleEvents(receiveCompletion: { result in
+            if case .failure = result {
+                logger.trace("Cache miss for url: \(url)")
+            }
+        })
+        .eraseToAnyPublisher()
+    }
+    
+    func logErrors(url: URL, logger: Logger) -> AnyPublisher<Output, Failure> {
+        return handleEvents(receiveCompletion: { result in
+            if case let .failure(error) = result {
+                logger.trace("Failed to loading url: \(url) with error: \(error.localizedDescription)")
+            }
+        })
+        .eraseToAnyPublisher()
+    }
+    
+    func logElapsedTime(url: URL, logger: Logger) -> AnyPublisher<Output, Failure> {
+        var startTime = CACurrentMediaTime()
+        return handleEvents(receiveSubscription: { _ in
+            startTime = CACurrentMediaTime()
+            logger.trace("Started loading url: \(url)")
+        }, receiveCompletion: { result in
+            let elasped = CACurrentMediaTime() - startTime
+            logger.trace("Finished loading url: \(url) in \(elasped) seconds")
+        })
+        .eraseToAnyPublisher()
     }
 }
