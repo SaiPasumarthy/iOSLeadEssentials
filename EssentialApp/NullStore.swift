91cc8f0d853e7f8a3a6510d85b7857d1ae308201
saipasumarthy <saipasumarthy009@gmail.com>
2024-01-06 14:11:04 +0530

    Make NullStore FeedImageDataStore implementation sync

--- EssentialApp/NullStore.swift
+++ EssentialApp/NullStore.swift
@@ -21,11 +21,11 @@ class NullStore: FeedStore & FeedImageDataStore {
         completion(.success(.none))
     }
     
-    func insert(data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-        completion(.success(()))
+    func insert(data: Data, for url: URL) throws {
+        
     }
     
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(.none))
-    }
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        .none
+    }    
 }
